a9ce6f814b19a107df03fc868a0fd5a663c13fa2
EleonorePS <103646636+EleonorePS@users.noreply.github.com>
2022-09-20 11:08:01 +0200

    Update SDR_geoprocessing_clean.py

--- SDR_geoprocessing_clean.py
+++ SDR_geoprocessing_clean.py
@@ -2,14 +2,9 @@
 """
 Created on Thu Jun 10 14:42:32 2021
 # author: Eleonore Pierrat
-# citation : Global modelling of water consumption impacts on freshwater fish biodiversity in LCA, submitted
-
+# citation : Pierrat, E., Barbarossa, V., Núñez, M., Scherer, L., Link, A., Damiani, M., … Dorber, M. (2023). Global water consumption impacts on riverine fish species richness in Life Cycle Assessment. Science of The Total Environment, 854, 158702. https://doi.org/10.1016/j.scitotenv.2022.158702
 
 this code georprocesses the river basins characteristics (aggregation at the river basin scale)
-
-
-
-
 
 @author: easpi
 """
@@ -41,14 +36,19 @@ os.chdir(Input.inputDir)
 
 #raster of river basins units
 spatial_unit = tifffile.imread(Input.inputDir + '/' + 'basin_5min_pcrglob_adjusted_resized.tif' )#pcrglob basin delineation
-#spatial_unit = tifffile.imread(Input.inputDir + '/' + 'gsim_BAF_MD.tif' )#gsim basin delineation - IT HAS A PROBLEM NOW
-spatial_unit = ma.masked_values(spatial_unit, -3.40282e+38)#some values are weird, they were filtered from the index array
+#spatial_unit = tifffile.imread(Input.inputDir + '/' + 'gsim_BAF_MD.tif' )#gsim basin delineation
+spatial_unit = ma.masked_values(spatial_unit, -3.40282e+38)#check  no data value
 
+#create filter
+index_filter=module.index_filter(spatial_unit)
+pointer_array=index_filter[0]
+idlist=index_filter[1]
 
 #array containing the coordinates of the grid cells included in each river basin
-pointer_array = np.load(Input.inputDir +'/'+ 'filter_array_catchments_valerio_adjusted.npy', allow_pickle = 1) 
-idlist= np.unique(spatial_unit)#does not work, select idlist from the fitler_Array
-idlist= idlist[3:,] 
+#np.save(Input.inputDir +'/'+ 'filter_array_catchments_valerio_adjusted.npy', allow_pickle = 1) 
+#pointer_array = np.load(Input.inputDir +'/'+ 'filter_array_catchments_valerio_adjusted.npy', allow_pickle = 1) 
+#idlist= np.unique(spatial_unit)
+#idlist= idlist[3:,] #removing no data areas
 
 #for gsim sensitivity
 #pointer_array = np.load(Input.inputDir +'/'+ 'filter_gsim_basins.npy', allow_pickle = 1)
@@ -82,31 +82,31 @@ tan_a = ma.masked_values(tan_a, 0,rtol = 1e-10, atol = 1e-10)
 h=module.plot_percentiles(tan_a)
 
 ti_map =np.log(area*1e6/tan_a)
-ti_agg = module.aggregate_average(ti_map, pointer_array, area)#careful that the area is in km2!!!
+ti_agg = module.aggregate_average(ti_map, pointer_array, area)#area is in km2!!!
 h=module.plot_percentiles(ti_agg)
 
 #discharge
 
 #simulated data including water consumption
-d  = Dataset(Input.outputDir + '/'+ 'discharge_outlet_human_1960_2010_pcrglob5.nc')
+d  = Dataset(Input.outputDir + '/'+ 'discharge_outlet_human_1960_2010_pcrglob5.nc')#import raw data
 lat = d.variables['latitude'][:]
 lon = d.variables['longitude'][:]
 q = d.variables['discharge flow'][:]#m3/s monthly
 q = np.append(q, np.full((1,q.shape[1]), 9.96921e+36), axis = 0)
 q = ma.masked_values(q, 9.96921e+36)
 
-q_yr = module.convert_month_to_year_sum(q*3600*24*365/12)#in m3/yr. average is wrong!! it has to bethe sum of monthly values to get annual values, this is the monthly average not yearly  average. rerun the whole thing
+q_yr = module.convert_month_to_year_sum(q*3600*24*365/12)#in m3/yr
 q_avg = np.mean(q_yr[:,-40:-10], axis = 1)#1970-2000 in m3/yr
 q_avg = q_avg/(365*24*3600) #m3/s annual average
 h=module.plot_percentiles(q_avg)
 
 #simulated data excluding water consumption (Q natural or Q0)
-d  = Dataset(Input.outputDir + '/'+ 'discharge_outlet_natural_1960-2010_pcrglob5.nc')
+d  = Dataset(Input.outputDir + '/'+ 'discharge_outlet_natural_1960-2010_pcrglob5.nc')#import raw data
 qn = d.variables['discharge flow'][:]#m3/s monthly
 qn = np.append(qn, np.full((1,qn.shape[1]), 9.96921e+36), axis = 0)
 qn = ma.masked_values(qn, 9.96921e+36)
 
-qn_yr = module.convert_month_to_year_sum(qn*3600*24*365/12)#in m3/yr. average is wrong!! it has to bethe sum of monthly values to get annual values, this is the monthly average not yearly  average. rerun the whole thing
+qn_yr = module.convert_month_to_year_sum(qn*3600*24*365/12)#in m3/yr.
 qn_avg = np.mean(qn_yr[:,-40:-10], axis = 1)#1970-2000 in m3/yr
 qn_avg = qn_avg/(365*24*3600) #m3/s annual average
 h=module.plot_percentiles(qn_avg)
@@ -114,7 +114,7 @@ h=module.plot_percentiles(qn_avg)
 
 #precipitation in the catchment 
 
-prec = tifffile.imread(Input.inputDir + '/' + 'wc2.1_5m_bio_12.tif' )#from world clim 2.1
+prec = tifffile.imread(Input.inputDir + '/' + 'wc2.1_5m_bio_12.tif' )#from world clim 2.1 #import raw data
 prec = ma.masked_values(prec, -3.4e+38)
 
 prec_pres_agg = module.aggregate_average(prec, pointer_array, area)
@@ -124,9 +124,9 @@ h=module.plot_percentiles(prec_pres_agg)
 
 
 #temperature in the catchment
-temp = tifffile.imread(Input.inputDir + '/' + 'wc2.1_5m_bio_1.tif' )
+temp = tifffile.imread(Input.inputDir + '/' + 'wc2.1_5m_bio_1.tif' ) #import raw data
 #temp = tifffile.imread('D:/SDR/CHELSA_cur_V1_2B_r5m/5min/bio_1_resized.tif' )
-temp = ma.masked_values(temp, -3.4e+38)#some values are weird, they were filtered from the index array
+temp = ma.masked_values(temp, -3.4e+38)
 temp = temp + 273.15
 temp_pres_agg = module.aggregate_average(temp, pointer_array, area)
 h = module.plot_percentiles(temp_pres_agg)
@@ -135,18 +135,17 @@ h = module.plot_percentiles(temp_pres_agg)
 
 #precipitation in last glacial maximum
 
-prec_21_a = tifffile.imread(Input.inputDir + '/' + 'cclgmbi12.tif' )
+prec_21_a = tifffile.imread(Input.inputDir + '/' + 'cclgmbi12.tif' ) #import raw data
 prec_21_a = np.append(prec_21_a, np.full((2160-prec_21_a.shape[0],4320), -3.40282e+38), axis = 0)
-prec_21_a = ma.masked_values(prec_21_a, -3.40282e+38)#some values are weird, they were filtered from the index array
+prec_21_a = ma.masked_values(prec_21_a, -3.40282e+38)
 
-prec_21_b = tifffile.imread(Input.inputDir + '/' + 'melgmbi12.tif' )
+prec_21_b = tifffile.imread(Input.inputDir + '/' + 'melgmbi12.tif' )#import raw data
 prec_21_b = np.append(prec_21_b, np.full((2160-prec_21_b.shape[0],4320), -3.40282e+38), axis = 0)
 prec_21_b = ma.masked_values(prec_21_b, -3.40282e+38)
-#some values are weird, they were filtered from the index array
 
-prec_21_c = tifffile.imread(Input.inputDir + '/' + 'mrlgmbi12.tif' )
+prec_21_c = tifffile.imread(Input.inputDir + '/' + 'mrlgmbi12.tif' )#import raw data
 prec_21_c = np.append(prec_21_c, np.full((2160-prec_21_c.shape[0],4320), -3.40282e+38), axis = 0)
-prec_21_c = ma.masked_values(prec_21_c, -3.40282e+38)#some values are weird, they were filtered from the index array
+prec_21_c = ma.masked_values(prec_21_c, -3.40282e+38)
 
 prec_21  = (prec_21_a + prec_21_b + prec_21_c)/3
 prec_21 = ma.masked_values(prec_21,0)
@@ -159,11 +158,11 @@ prec_var = (prec - prec_21)/prec_21
 prec_21_agg = module.aggregate_average(prec_var, pointer_array, area)
 h = module.plot_percentiles(prec_21_agg)
 
-bug = ma.masked_where(prec_21_agg>1, prec_21_agg)#showing only where prec_21_agg is correct
-bug_map = module.make_map(bug, pointer_array, idlist, spatial_unit)
-module.new_map_netcdf(Input.outputDir + '/'+ 'prec_delta_bug_map', bug_map, 'pred_delta', '1', lat, lon)
-plt.imshow(bug_map)#seems ok
-plt.imshow(spatial_unit)
+#bug = ma.masked_where(prec_21_agg>1, prec_21_agg)#showing only where prec_21_agg is correct
+#bug_map = module.make_map(bug, pointer_array, idlist, spatial_unit)
+#module.new_map_netcdf(Input.outputDir + '/'+ 'prec_delta_bug_map', bug_map, 'pred_delta', '1', lat, lon)
+#plt.imshow(bug_map)#seems ok
+#plt.imshow(spatial_unit)
 
 
 
@@ -172,23 +171,23 @@ plt.imshow(spatial_unit)
 
 #temperature in the last glacial maximum
  
-temp_21_a = tifffile.imread(Input.inputDir + '/' + 'cclgmbi1.tif' )
+temp_21_a = tifffile.imread(Input.inputDir + '/' + 'cclgmbi1.tif' )#import raw data
 temp_21_a = np.append(temp_21_a, np.full((2160-temp_21_a.shape[0],4320), -3.40282e+38), axis = 0)
-temp_21_a = ma.masked_values(temp_21_a, -3.40282e+38)#some values are weird, they were filtered from the index array
-temp_21_a = temp_21_a/10 + 273.15
+temp_21_a = ma.masked_values(temp_21_a, -3.40282e+38)
+temp_21_a = temp_21_a/10 + 273.15 #in degree celsius
 
-temp_21_b = tifffile.imread(Input.inputDir + '/' + 'melgmbi1.tif' )
+temp_21_b = tifffile.imread(Input.inputDir + '/' + 'melgmbi1.tif' )#import raw data
 temp_21_b = np.append(temp_21_b, np.full((2160-temp_21_b.shape[0],4320), -3.40282e+38), axis = 0)
-temp_21_b = ma.masked_values(temp_21_b, -3.40282e+38)#some values are weird, they were filtered from the index array
-temp_21_b = temp_21_b/10 + 273.15
+temp_21_b = ma.masked_values(temp_21_b, -3.40282e+38)
+temp_21_b = temp_21_b/10 + 273.15 #in Kelvin
 
-temp_21_c = tifffile.imread(Input.inputDir + '/' + 'mrlgmbi1.tif' )
+temp_21_c = tifffile.imread(Input.inputDir + '/' + 'mrlgmbi1.tif' )#import raw data
 temp_21_c = np.append(temp_21_c, np.full((2160-temp_21_c.shape[0],4320), -3.40282e+38), axis = 0)
-temp_21_c = ma.masked_values(temp_21_c, -3.40282e+38)#some values are weird, they were filtered from the index array
-temp_21_c = temp_21_c/10 + 273.15
+temp_21_c = ma.masked_values(temp_21_c, -3.40282e+38)
+temp_21_c = temp_21_c/10 + 273.15 #in Kelvin
 
 
-temp_21  = (temp_21_a  + temp_21_b + temp_21_c)/3#variation
+temp_21  = (temp_21_a  + temp_21_b + temp_21_c)/3
 temp_21 = ma.masked_values(temp_21, 0)
 
 temp_var = (temp-temp_21)/temp_21
@@ -227,12 +226,12 @@ climate_5 = ma.masked_equal(out,0)
 
 #freshwater ecoregion, habitat, and realm 
 
-fer = tifffile.imread(Input.inputDir + '/' + 'FER.tif' )
+fer = tifffile.imread(Input.inputDir + '/' + 'FER.tif' )#import raw data
 fer = ma.masked_values(fer, 0)
 ecoregion = module.attribute_label(fer, pointer_array)
 
 #upload attribute table (extracted manually from  https://www.feow.org/)
-table = pd.read_excel(Input.inputDir + '/'+'freshwater_ecoregions.xlsx')
+table = pd.read_excel(Input.inputDir + '/'+'freshwater_ecoregions.xlsx')#import raw data
 
 out = np.full((ecoregion.shape[0],3), 1e20)
 
@@ -304,7 +303,7 @@ df = pd.DataFrame(data,columns=[
 
 #%% #add species richness 
 # #not needed for sensitivity
-table_sr = pd.read_excel(Input.inputDir + '/'+'pcrglobwb_5min_basins_SR.xlsx')
+table_sr = pd.read_excel(Input.inputDir + '/'+'pcrglobwb_5min_basins_SR.xlsx')#import raw data
 table_sr.columns =['id_basin_pcrglob',"SR_tot","SR_exo"]
 
 df_join = df.set_index('id_basin_pcrglob').join(table_sr.set_index('id_basin_pcrglob'))
@@ -345,7 +344,7 @@ np.min(df_join['climate5'])
 
 df_f = df_join[df_join['area']>threshold_area]#(4064) the rest is small basin
 df_f.shape
-np.sum(df_f['area'])/np.sum(df_join['area'])
+np.sum(df_f['area'])/np.sum(df_join['area'])#% landmass coverage
 
 df_f =df_f[df_f['q']>0]#(3931)
 df_f=df_f[df_f['SR_tot']>=0]
@@ -356,6 +355,7 @@ np.sum(df_f['area'])/np.sum(df_join['area'])#88% landcover has valid data
 
 #stats.scoreatpercentile(df['q'],10)
 
+#data distribution
 h=module.plot_percentiles(df_f['SR_tot'])#km3/yr
 h=module.plot_percentiles(df_f['q'])#m3/s annual average
 h=module.plot_percentiles(df_f['qnat'])#m3/s annual average
@@ -378,7 +378,7 @@ for i in df_f.index:#id basin pcrglob inlcuded in the scope
 mask = ma.masked_values(mask, 0)
 np.sum(mask)#ok
 
-map_scope = module.make_map(mask, pointer_array, idlist, spatial_unit)#broken
+map_scope = module.make_map(mask, pointer_array, idlist, spatial_unit)
 plt.imshow(map_scope)
 #export map scope
 module.new_map_netcdf(Input.outputDir + '/'+ 'map_scope', map_scope, 'id', '1', lat, lon)
